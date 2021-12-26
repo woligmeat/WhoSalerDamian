eadbf62fb96081a2547bb0e62d6ef18b28b09bb0
Damian Pedrycz <damian.pedrycz@onet.eu>
2021-12-26 16:48:01 +0100

    Added Login and Register Pages

--- WhoSalerDamian/obj/Debug/net5.0/Razor/Areas/Identity/Pages/_ViewStart.cshtml.g.cs
+++ WhoSalerDamian/obj/Debug/net5.0/Razor/Areas/Identity/Pages/_ViewStart.cshtml.g.cs
@@ -12,7 +12,29 @@ namespace AspNetCore
     using Microsoft.AspNetCore.Mvc;
     using Microsoft.AspNetCore.Mvc.Rendering;
     using Microsoft.AspNetCore.Mvc.ViewFeatures;
+#nullable restore
+#line 1 "/Users/damianpedrycz/RiderProjects/WhoSalerDamian/WhoSalerDamian/Areas/Identity/Pages/_ViewImports.cshtml"
+using Microsoft.AspNetCore.Identity;
+
+#line default
+#line hidden
+#nullable disable
+#nullable restore
+#line 2 "/Users/damianpedrycz/RiderProjects/WhoSalerDamian/WhoSalerDamian/Areas/Identity/Pages/_ViewImports.cshtml"
+using WhoSalerDamian.Areas.Identity;
+
+#line default
+#line hidden
+#nullable disable
+#nullable restore
+#line 3 "/Users/damianpedrycz/RiderProjects/WhoSalerDamian/WhoSalerDamian/Areas/Identity/Pages/_ViewImports.cshtml"
+using WhoSalerDamian.Areas.Identity.Pages;
+
+#line default
+#line hidden
+#nullable disable
     [global::Microsoft.AspNetCore.Razor.Hosting.RazorSourceChecksumAttribute(@"SHA1", @"3ad611fb2a9efe5e41f1bfb4946ec09d55baed48", @"/Areas/Identity/Pages/_ViewStart.cshtml")]
+    [global::Microsoft.AspNetCore.Razor.Hosting.RazorSourceChecksumAttribute(@"SHA1", @"8db5ab136fc2d9bd608c77ef51019e7b6efb613f", @"/Areas/Identity/Pages/_ViewImports.cshtml")]
     public class Areas_Identity_Pages__ViewStart : global::Microsoft.AspNetCore.Mvc.Razor.RazorPage<dynamic>
     {
         #pragma warning disable 1998
